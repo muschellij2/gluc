7742bfbc45c2ce7c8498eb23af6742f55578df21
muschellij2 <muschellij2@gmail.com>
2017-04-04 17:48:51 -0400

    Adding all the things!

---
+++ R/read_abbott.R
@@ -0,0 +1,24 @@
+#' @title Read Abbott Output Data
+#' @description Reads an excel file with Abbot Raw data
+#'
+#' @param path Path of .xlsx file.  Must have a sheet named "AbbottRaw" in it
+#'
+#' @return If the sheet is not empty, it will return a \code{data.frame} of
+#' values.  Otherwise, it will return \code{NULL}
+#' @export
+#' @importFrom readxl read_excel
+read_abbott = function(path) {
+  res = read_excel(path = path,
+                   sheet = "AbbottRaw",
+                   col_names = FALSE)
+  if (all(is.na(res))) {
+    return(NULL)
+  }
+  id_row = as.data.frame(res)[1,1]
+  res = res[-c(1:2),]
+  colnames(res) = as.character(res[1,])
+  res = res[-1,]
+  attr(res, "id_value") = id_row
+  # res = as.data.frame(res)
+  return(res)
+}
