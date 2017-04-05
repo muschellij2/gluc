037b243d6600ed0d5efd50276ca97516411b9941
muschellij2 <muschellij2@gmail.com>
2017-04-05 10:30:34 -0400

    Added minimum number of rows.

--- R/read_gluc.R
+++ R/read_gluc.R
@@ -4,16 +4,19 @@
 #'
 #' @param path path of xlsx file
 #' @param type type of file to read
+#' @param min_rows Minimum number of rows
 #' @param ... additional argument to send to the read function related to
 #' \code{type}
 #'
 #' @return A \code{tbl} data.frame type.
 #' @importFrom pbapply pblapply
+#' @importFrom dplyr group_by tally left_join filter arrange
 #' @export
 read_gluc = function(
   path,
   type = c("Abbott", "Dexcom",
            "AbbottRaw", "DexcomRaw"),
+  min_rows = 5,
   ...){
 
   type = match.arg(type)
@@ -46,6 +49,16 @@ read_gluc = function(
     })
     res = data.table::rbindlist(res, fill = TRUE)
     res = as.data.frame(res)
+    res$RERUN_INDEX = seq(nrow(res))
+    res = dplyr::group_by(.data = res, file)
+    tal = dplyr::tally(res)
+    colnames(tal) = c("file", "NUMBER_OF_TIME_POINTS")
+    res = dplyr::left_join(res, tal, by = file)
+    res = dplyr::filter(res, NUMBER_OF_TIME_POINTS >= min_rows)
+    res$NUMBER_OF_TIME_POINTS = NULL
+    res = dplyr::arrange(res, RERUN_INDEX)
+    res$RERUN_INDEX = NULL
+
   }
 
   if (!is.null(res)) {
